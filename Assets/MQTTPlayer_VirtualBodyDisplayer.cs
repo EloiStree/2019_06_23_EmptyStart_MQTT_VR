7c5f1d3e37526fc83bb65caf9221ac70fece7434
Strée Eloi <jams.center@gmail.com>
2019-06-23 13:35:52 +0100

    Start to work. Need to make the code works for several user in next steps

---
+++ Assets/MQTTPlayer_VirtualBodyDisplayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MQTTPlayer_VirtualBodyDisplayer : MonoBehaviour
+{
+    public string linkedUserId;
+    public GameObject m_head;
+    public GameObject m_leftHand;
+    public GameObject m_rightHand;
+    public MQTT_UserPackageReceived m_last;
+    public VirtualBodyPosition m_lastBodyPosition;
+    public float m_latency;
+    public bool m_useLerp;
+
+    public void ReceivedMDTTPackage(MQTT_UserPackageReceived package) {
+
+        if(package.m_userDeviceId== linkedUserId)
+            if (package.GetTimeWhenSendAsTimestamp() >= m_last.GetTimeWhenSendAsTimestamp()){
+                m_last = package;
+                m_latency = m_last.GetLatency();
+                m_lastBodyPosition = JsonUtility.FromJson<VirtualBodyPosition>(package.m_message);
+            
+            }
+
+    }
+    public void Update()
+    {
+
+        SetWith(m_lastBodyPosition);
+    }
+
+    void SetWith(VirtualBodyPosition value)
+    {
+        if (m_useLerp)
+        {
+            Lerp(m_head, value.m_head);
+            Lerp(m_leftHand, value.m_leftHand);
+            Lerp(m_rightHand, value.m_rightHand);
+
+        }
+        else {
+            m_head.transform.position = value.m_head.m_unityPointOfView.m_position;
+            m_head.transform.rotation = value.m_head.m_unityPointOfView.m_rotation;
+
+            m_leftHand.transform.position = value.m_leftHand.m_unityPointOfView.m_position;
+            m_leftHand.transform.rotation = value.m_leftHand.m_unityPointOfView.m_rotation;
+
+            m_rightHand.transform.position = value.m_rightHand.m_unityPointOfView.m_position;
+            m_rightHand.transform.rotation = value.m_rightHand.m_unityPointOfView.m_rotation;
+        }
+    }
+
+    private void Lerp(GameObject target,  SpaceTrackedPoint value)
+    {
+        target.transform.position = Vector3.Lerp(target.transform.position, value.m_unityPointOfView.m_position, Time.deltaTime* m_lerpFactor);
+        target.transform.rotation = Quaternion.Lerp(target.transform.rotation, value.m_unityPointOfView.m_rotation, Time.deltaTime* m_lerpFactor);
+    }
+    public float m_lerpFactor=5;
+}
--- Assets/UserMQTT.cs
+++ Assets/UserMQTT.cs
@@ -13,25 +13,32 @@ public class UserMQTT : MonoBehaviour
 
     [Header("MQTT")]
     public string m_serverIp = "143.185.118.233";
-    public int m_serverPort = 8080;
+    public int m_serverPort = 1883;
     public List<string> m_listenedTopics ;
     private MqttClient client;
     public bool m_useDebug = false;
+    public bool m_useDebugLog = false;
 
     public float m_timeSincePlaying;
     public float m_bytesSend;
 
+    public Dictionary<string, MQTT_UserPackageReceived> m_lastPackage = new Dictionary<string, MQTT_UserPackageReceived>();
+    public Dictionary<string, bool> m_lastPackageReceivedCheck = new Dictionary<string, bool>();
+    public Dictionary<string, string> m_userDetectedOnceRegister = new Dictionary<string, string>();
+
+
+    [Header("Debug info")]
+    public List<string> m_userDetectedOnce;
+    public List<string> m_userByTopic;
+
+    public OnNewUserDetected m_newUser;
+    public OnPackageReceivedEvent m_debugPackage;
     internal void AddTopicToListen(string topic)
     {
         //m_listenedTopics.Add(topic);
         //client.Subscribe(m_listenedTopics.ToArray(), new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
     }
-
-    public OnPackageReceivedEvent m_debugPackage;
-
-    public Queue<MQTT_UserPackageReceived> m_receivedByThread = new Queue<MQTT_UserPackageReceived>();
-
-     void Awake()
+    void Awake()
     {
         m_deviceId = GetUserId();
         client = new MqttClient(IPAddress.Parse(m_serverIp), m_serverPort, false, null);
@@ -53,25 +60,52 @@ public class UserMQTT : MonoBehaviour
             int endPackageInfo = message.IndexOf("<HeyMonAmi/>");
             int messageStart = endPackageInfo + "<HeyMonAmi/>".Length;
 
-            string packageInfo = message.Substring(startPackageInfo, endPackageInfo);
+            string packageInfo = message.Substring(startPackageInfo,endPackageInfo- startPackageInfo);
             string[] infoToken = packageInfo.Split('|');
-            MQTT_UserPackageReceived info = new MQTT_UserPackageReceived();
-            info.m_userDeviceId = infoToken[0];
-            info.m_timestampWhenSend = infoToken[1];
+            string userId = infoToken[0];
+            string timeWhenSend=infoToken[1];
+            string packageKey = e.Topic + "|" + userId;
+            if (m_lastPackage.ContainsKey(packageKey))
+            {
+                if (m_lastPackage[packageKey].IsPackageMoreRecentThat(timeWhenSend)) {
+                    return;
+                }
+            }
+            else m_lastPackage[packageKey] = new MQTT_UserPackageReceived();
+
+            if (! m_userDetectedOnceRegister.ContainsKey(userId)) {
+                m_newUser.Invoke(userId);
+            }
+            m_userDetectedOnceRegister[userId] = userId;
+            MQTT_UserPackageReceived info = m_lastPackage[packageKey];
+            info.m_userDeviceId = userId;
+            info.m_timestampWhenSend = timeWhenSend;
             info.m_timestampWhenReceived =""+ GetTime();
             info.m_message = message.Substring(messageStart);
             info.m_topic = e.Topic;
-            Debug.Log("Sender: "+info.m_userDeviceId +" ?? " + sender.ToString());
-            Debug.Log("Received: " + info.m_message);
-            Debug.Log("ID: : " + client.ClientId);
-            m_receivedByThread.Enqueue(info);
+            if (m_useDebugLog) {
+
+                Debug.Log("Sender: "+info.m_userDeviceId +" ?? " + sender.ToString());
+                Debug.Log("Received: " + info.m_message);
+                Debug.Log("ID: : " + client.ClientId);
+            }
+           // m_lastPackage[packageKey] = info;
+            m_lastPackageReceivedCheck[packageKey] = true;
         }
     }
     private void Update()
     {
-        while (m_receivedByThread.Count > 0) {
-            MQTT_UserPackageReceived info = m_receivedByThread.Dequeue();
-            m_debugPackage.Invoke(info);
+
+        m_userDetectedOnce = new List<string>(this.m_userDetectedOnceRegister.Keys);
+
+        List<string> keyList = new List<string>(this.m_lastPackageReceivedCheck.Keys);
+        m_userByTopic = keyList;
+        foreach (string key in keyList)
+        {
+            if (m_lastPackageReceivedCheck[key]) {
+                m_lastPackageReceivedCheck[key] = false;
+                m_debugPackage.Invoke(m_lastPackage[key]);
+            }
         }
         m_timeSincePlaying += Time.deltaTime;
     }
@@ -101,13 +135,70 @@ public class UserMQTT : MonoBehaviour
 
     
 }
-public struct MQTT_UserPackageReceived
+[System.Serializable]
+public class MQTT_UserPackageReceived
 {
     public string m_timestampWhenSend;
     public string m_timestampWhenReceived;
     public string m_userDeviceId;
     public string m_topic;
     public string m_message;
+
+   
+
+    internal float GetLatency()
+    {
+        return (GetTimeWhenReceivedAsTimestamp() - GetTimeWhenSendAsTimestamp())/10000000f;
+    }
+    public string GetUserDeviceId() {
+        return m_userDeviceId;
+    }
+    internal long GetTimeWhenReceivedAsTimestamp()
+    {
+        try
+        {
+            return long.Parse(m_timestampWhenReceived);
+        }catch (Exception) { return 0; }
+    }
+    internal long GetTimeWhenSendAsTimestamp()
+    {
+        try {
+            return long.Parse(m_timestampWhenSend);
+        }
+        catch (Exception) { return 0; }
+    }
+
+    internal bool IsNotDefined()
+    {
+        return m_timestampWhenSend == "" || m_timestampWhenReceived == "";
+    }
+
+    internal bool IsPackageOlderThat(string timeWhenSend)
+    {
+        try
+        {
+            long currentPackage = long.Parse(m_timestampWhenSend);
+            long givenPackage = long.Parse(timeWhenSend);
+            return currentPackage < givenPackage;
+        }
+        catch (Exception) { return false; }
+    }
+
+    internal bool IsPackageMoreRecentThat(string timeWhenSend)
+    {
+        try
+        {
+            long currentPackage = long.Parse(m_timestampWhenSend);
+            long givenPackage = long.Parse(timeWhenSend);
+            return currentPackage > givenPackage;
+        }
+        catch (Exception) { return false; }
+    }
 }
+
+
+[System.Serializable]
+public class OnNewUserDetected : UnityEvent<string> { };
+
 [System.Serializable]
 public class OnPackageReceivedEvent : UnityEvent<MQTT_UserPackageReceived> { };
